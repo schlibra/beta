a89225c508dbbed732b4cf314f0acedb7c25b119
schlibra <39517786+schlibra@users.noreply.github.com>
2019-02-11 08:21:12 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+function dh(id){
+	home.className="normal";
+	home_page.className="normal_page";
+	application.className="normal";
+	application_page.className="normal_page";
+	article.className="normal";
+	article_page.className="normal_page";
+	other.className="normal";
+	other_page.className="normal_page";
+	catchus.className="normal";
+	catchus_page.className="normal_page";
+	document.getElementById(id).className="active";
+	document.getElementById(id+"_page").className="active_page";
+}
